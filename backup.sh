c15818c668b012b811e9789caef42e351485ea78
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-05-02 10:28:32 -0300

    validação ao criar diretorio

--- backup.sh
+++ backup.sh
@@ -14,9 +14,9 @@ backup_path="/home/jesher/claretiano/tickets" # Diretório de backup
 
 backup_path2="/home/jesher/Documentos"
 
-external_storage="/media/jesher/Ventoy" # Dispositivo externo de destino
+external_storage="/media/jesher/JESHER" # Dispositivo externo de destino
 
-create="/media/jesher/Ventoy/backups" # Diretório para ser criado dentro do dispositivo externo
+destination="/media/jesher/JESHER/backups" # Diretório para ser criado dentro do dispositivo externo
 
 date_log="$(date)"
 
@@ -28,7 +28,8 @@ final_archive2="claretiano_documentos-$date_format.tar.gz"
 
 log_file="/var/log/daily-backup.log" # Arquivo de log
 
-# tail -f /var/log/daily-backup.log ou less +F /var/log/daily-backup.log | para monitorar o arquvo de log
+# Monitorar o arquvo de log
+# tail -f /var/log/daily-backup.log ou less +F /var/log/daily-backup.log
 
 # --- TESTES
 
@@ -36,23 +37,22 @@ if ! mountpoint -q -- $external_storage; then # Pendrive plugado na máquina?
 	printf "[$date_log] DEVICE NOT MOUNTED in: $external_storage CHECK IT.\n" >> $log_file
 	exit 1
 else 
-	mkdir -p ${create} #fazer validação parar quando já criado, não fazer nada
+	[[ ! -d "$destination" ]] && mkdir -p "$destination"
 fi
 
 # --- EXECUÇÃO
 
-if tar -cpSzf "$create/$final_archive" "$backup_path" &>/dev/null; then
+if tar -cpSzf "$destination/$final_archive" "$backup_path" &>/dev/null; then
 	printf "[$date_log] BACKUP SUCCESS.\n" >> $log_file
 else
 	printf "[$date_log] BACKUP ERROR...!!\n" >> $log_file
 fi
 
 # Execução do backup_path2
-if tar -cpSzf "$create/$final_archive2" "$backup_path2" &>/dev/null; then
+if tar -cpSzf "$destination/$final_archive2" "$backup_path2" &>/dev/null; then
 	printf "[$date_log] BACKUP SUCCESS - DIRETÓRIO DOCUMENTOS PC DO TRAMPO.\n" >> $log_file
 else
 	printf "[$date_log] BACKUP ERROR - DIRETÓRIO DOCUMENTOS PC DO TRAMPO.\n" >> $log_file
 fi
 
-#find ${external_storage}*.gz -mtime +3 -delete # Excluir arquivos com mais de dez dias
-# Arrumar aqui para deletar somente arquivos de backup.tar.gz
+find ${destination}/*.gz -mtime +3 -delete # Excluir arquivos tar.gz com mais de três dias
