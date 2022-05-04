115ef0a0755f1a2d91bb62b992b12ce676701f4d
Jesher Minelli <jesheralves@claretiano.edu.br>
2022-05-04 13:25:48 -0300

    organizando variaveis

--- backup.sh
+++ backup.sh
@@ -11,23 +11,14 @@
 # --- VARIÁVEIS
 
 backup_path="/home/jesher/claretiano/tickets" # Diretório de backup
-
 backup_path2="/home/jesher/Documentos"
-
 external_storage="/media/jesher/JESHER" # Dispositivo externo de destino
-
 destination="/media/jesher/JESHER/backups" # Diretório para ser criado dentro do dispositivo externo
-
 date_log="$(date)"
-
 date_format=$(date "+%A %d-%m-%Y")
-
 final_archive="backup-$date_format.tar.gz" # Formato do arquivo
-
 final_archive2="claretiano_documentos-$date_format.tar.gz"
-
 log_file="/var/log/daily-backup.log" # Arquivo de log
-
 # Monitorar o arquvo de log
 # tail -f /var/log/daily-backup.log ou less +F /var/log/daily-backup.log
 
